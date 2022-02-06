f069b4653398487117b9f9f2fe9324f182cbee6a
Sterling Schulkins <sterling.schulkins@gmail.com>
2022-02-05 20:37:17 -0600

    trying an alternate fix for msvc

--- Nanoreflect.h
+++ Nanoreflect.h
@@ -26,6 +26,11 @@ SOFTWARE.
 #include <map>
 #include <iterator>
 #include <vector>
+//#include <memory>
+
+#ifdef _MSC_VER
+#  include "typeinfo" //cannot use angle brackets here...
+#endif
 
 #if _MSC_VER >= 1200
 // visual studio specific compiler warnings
@@ -36,10 +41,13 @@ SOFTWARE.
 #pragma warning(disable : 4302)
 #endif
 
+
+#pragma pack(show)
+
 namespace nanoreflect {
 
   template <typename T>
-  struct TypeDescriptor;
+  struct TypeDescriptor;  
   
   template <typename T>
   TypeDescriptor<T>* GetTypeDescriptor() {
@@ -49,32 +57,78 @@ namespace nanoreflect {
   }
   
   struct Member;
+  static void DeleteMember(Member* member);
   struct TypeDescriptorData {
     const char* type_name; // string name of this type
     size_t size; // size in bytes of this type in memory
     unsigned int type_id; // unique type id of this type, determined at runtime only
-    std::vector<Member> members;
+    std::vector<Member*> members;
+    TypeDescriptorData() {
+      this->type_name = nullptr;
+      this->type_id = 0;
+      this->size = 0;
+    }
+    TypeDescriptorData(const TypeDescriptorData* other) {
+      this->type_name = other->type_name;
+      this->size = other->size;
+      this->type_id = other->type_id;
+    }
+    ~TypeDescriptorData() {
+      for (int i = 0; i < members.size(); ++i) {
+        DeleteMember(members[i]);
+      }
+    }
   };
-
+    
   struct Member {
     size_t ordinal; // order this member appears in its containing class
     size_t offset; // byte offset of this member in memory from the start of its containing class
-    TypeDescriptorData type_data;
+    TypeDescriptorData* type_data;
     const char* name; // the name of the member in its containing class
     void* type_descriptor; // pointer to the type descriptor for this member, this can be cast to TypeDescriptor<T>* type
+    Member(size_t ordinal, size_t offset, TypeDescriptorData* other_type_data, const char* name, void* type_descriptor) {
+      this->ordinal = ordinal;
+      this->offset = offset;
+      this->type_data = new TypeDescriptorData(other_type_data);
+      // we have to help the TypeDescriptorData copy its members because it doesn't know about the Member class structure due to C++ cyclical dependency limitations
+      for (int i = 0; i < other_type_data->members.size(); ++i) {
+        Member* other_m = other_type_data->members[i];
+        Member* m = new Member(other_m->ordinal, other_m->offset, other_m->type_data, other_m->name, other_m->type_descriptor);
+        this->type_data->members.push_back(m);
+      }
+      this->name = name;
+      this->type_descriptor = type_descriptor;
+    }
+
+    ~Member() {
+      delete type_data;
+    }
   };
+
+  // this is only to get around cyclic dependency issues since we're in a single header
+  static void DeleteMember(Member* member) {
+    delete member;
+  }
+  
 
   template <typename T> // T is the type this TypeDescriptor represents
   struct TypeDescriptor {
         
-    TypeDescriptorData type_data;
+    TypeDescriptorData* type_data;
     // lookup that maps offsets to the corresponding Member index. since each member will have a unique offset
     std::map<size_t, size_t> offset_to_member_ordinal;
     
-    TypeDescriptor() {      
-      type_data.type_name = typeid(T).name();
-      type_data.size = sizeof(T);
-      type_data.type_id = reinterpret_cast<unsigned int>(this);      
+    TypeDescriptor() { 
+      type_data = new TypeDescriptorData();
+#ifdef _MSC_VER
+      using ::type_info;
+#endif
+      type_data->type_name = typeid(T).name();
+      type_data->size = sizeof(T);
+      type_data->type_id = reinterpret_cast<unsigned int>(this);      
+    }
+    ~TypeDescriptor() {
+      delete type_data;
     }
     
     // Only one instance of this class should ever exist in memory. Do not allow copying at all.
@@ -100,9 +154,9 @@ namespace nanoreflect {
       static T object{}; // this can also be constexpr instead of static but that limits us to constexpr constructors
       size_t offset = size_t(&(object.*member)) - size_t(&object);
       TypeDescriptor<TM> *member_type_desc = GetTypeDescriptor<TM>();      
-      Member m{ this->type_data.members.size(), offset, member_type_desc->type_data, member_name, member_type_desc };
-      this->type_data.members.push_back(m);
-      offset_to_member_ordinal[offset] = m.ordinal;
+      Member* m = new Member( this->type_data->members.size(), offset, member_type_desc->type_data, member_name, member_type_desc );
+      this->type_data->members.push_back(m);
+      offset_to_member_ordinal[offset] = m->ordinal;
     }
   };
   
@@ -117,11 +171,12 @@ struct type_name ## _static_typedescriptor_constructor { \
 
 #define REFLECTED_OBJECT_END(type_name) \
   } \
-} Static_instance_typedescriptor_constructor_ ## type_name;
+} static Static_instance_typedescriptor_constructor_ ## type_name;
 
 };
 #if _MSC_VER >= 1200
 // visual studio specific compiler warnings
 #pragma warning ( pop )
 #endif
+
 #endif
